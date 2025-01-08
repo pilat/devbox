7e0b3b6695f67e10ea93a11173d9a7014f0078e4
Vladimir Urushev <vladimir.urushev@mondu.ai>
2025-01-08 14:44:06 +0100

    Add version info command

---
+++ cmd/devbox/version.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+var (
+	version    string
+	commit     string
+	commitDate string
+)
+
+func init() {
+	cmd := &cobra.Command{
+		Use:   "version",
+		Short: "Print the version number of devbox",
+		Long:  "That command will print the version number of devbox",
+		Args:  cobra.MinimumNArgs(0),
+		RunE: runWrapper(func(ctx context.Context, cmd *cobra.Command, args []string) error {
+			fmt.Printf("Version: %s\nCommit: %s\nCommit date: %s\n", version, commit, commitDate)
+
+			return nil
+		}),
+	}
+
+	_ = cmd.Flags().MarkHidden("name")
+
+	root.AddCommand(cmd)
+}
